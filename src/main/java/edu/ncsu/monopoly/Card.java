0a57396ef5f27be4452571eadd1f16f59a20ba1d
velez <velez@10.245.50.120>
2026-09-25 10:48:07 +0200

    Card comment added

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -1,11 +1,14 @@
 package edu.ncsu.monopoly;
-//Eguneraketa irailak 25 bigarrena
+
+//I have added a comment
 public abstract class Card {
 
-    public static final int TYPE_CHANCE = 1;
-    public static final int TYPE_CC = 2;
+	public static final int TYPE_CHANCE = 1;
+	public static final int TYPE_CC = 2;
 
-    public abstract String getLabel();
-    public abstract void applyAction();
-    public abstract int getCardType();
+	public abstract String getLabel();
+
+	public abstract void applyAction();
+
+	public abstract int getCardType();
 }
